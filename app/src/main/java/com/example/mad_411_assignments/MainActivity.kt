2c3476648875bfcdd3da0686f226bf0b7341e898
Kohinoor Jeet Singh <w0859774@myscc.ca>
2025-02-26 19:05:16 -0500

    Created a nex Activity with a bucnch of views added all the views and button and the function to make the button work

--- app/src/main/java/com/example/mad_411_assignments/MainActivity.kt
+++ app/src/main/java/com/example/mad_411_assignments/MainActivity.kt
@@ -1,20 +1,30 @@
 package com.example.mad_411_assignments
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var textNameView: EditText
+    private lateinit var textNameTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        textNameView = findViewById(R.id.text_attend_edit_text)
+        textNameTextView = findViewById(R.id.show_name_text)
+    }
+
+    fun ShowName(view: View) {
+        val textAttendStr = textNameView.text.toString()
+
+
+        textNameTextView.text = "Hello, $textAttendStr"
     }
 }
